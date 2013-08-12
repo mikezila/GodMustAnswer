525145259dec02a689bdf9ca60da1a5a0eda9430
mikezila <donoteatmikezila@gmail.com>
2013-08-12 15:40:35 -0400

    feet now work

--- rb/ball.rb
+++ rb/ball.rb
@@ -6,7 +6,7 @@ class Ball < Mob # < GameObject
 
   def initialize(game,origin)
     super game, origin
-    @jumping = @grounded = false
+    @jumping = @grounded = @block_left = @block_right = @block_above = false
     self.tags.push("ball")
     @gfx = Gosu::Image.new($window, gfx("ball"), false)
     @box = generate_box
@@ -27,18 +27,20 @@ class Ball < Mob # < GameObject
   # I don't like this.  Using a single "foot" point is a bad idea.
   # Maybe two will work better?
   def check_col
+    # Clean up from last run, so collision detection can begin anew.
     cannidates = Array.new
-    cannidates.clear
     @grounded = false
 
+    # Find all the objects we're touching, so we can work with them instead of every gameobject.
     self.game.objects.each do |object|
       if self.box.collides?(object.box)
         cannidates.push(object)
       end
     end
 
+    # Check to see if we're standing on any of them.
     cannidates.each do |cannidate|
-      if self.box.left_foot.x > cannidate.box.origin.x or self.box.right_foot.x < cannidate.box.origin.x + cannidate.box.width
+      if self.box.left_foot.x > cannidate.box.origin.x and self.box.left_foot.x < cannidate.box.origin.x + cannidate.box.width or self.box.right_foot.x < cannidate.box.origin.x + cannidate.box.width and self.box.right_foot.x > cannidate.box.origin.x
         @grounded = true
         debug("grounded")
       end
